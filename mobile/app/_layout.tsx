e701f6b12efbe1ffa7b6f7a650446d73c91d52d3
consuliam <consuliam@gmail.com>
2023-06-10 16:17:05 -0300

    mobile layout defined, index route updated, new memory page created

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,63 @@
+import { styled } from 'nativewind'
+import { ImageBackground } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import { SplashScreen, Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
+import { useEffect, useState } from 'react'
+
+const StyledStripes = styled(Stripes)
+
+export default function Lauoyt() {
+  const [isUserAuthenticated, setIsUserAutheticated] = useState<null | boolean>(
+    null,
+  )
+
+  const [hasLoadedFont] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAutheticated(!!token)
+    })
+  }, [])
+
+  if (!hasLoadedFont) {
+    return <SplashScreen />
+  }
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+      <StatusBar style="light" translucent />
+
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: 'transparent' },
+        }}
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="new" />
+        <Stack.Screen name="memories" />
+      </Stack>
+    </ImageBackground>
+  )
+}
